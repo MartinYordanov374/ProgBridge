77bfed9f510f2def5e2fe715a3b6e2b2a6a15ebd
GoofyGalaxy374 <prophetyordanov@gmail.com>
2022-03-25 22:24:31 +0200

    The logged in user can no longer see the login and register links

--- src/app/navbar/navbar.component.ts
+++ src/app/navbar/navbar.component.ts
@@ -7,8 +7,9 @@ import { Component, OnInit } from '@angular/core';
 export class NavbarComponent implements OnInit {
 
   constructor() { }
-
+  isUser: any ;
   ngOnInit(): void {
+    this.isUser = localStorage.getItem('user')
   }
 
 }
