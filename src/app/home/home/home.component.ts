c2cd336d19d9ecc644afcfa0c2087ba421948a58
GoofyGalaxy374 <prophetyordanov@gmail.com>
2022-03-31 12:25:14 +0300

    Added character counter to the post field

--- src/app/home/home/home.component.ts
+++ src/app/home/home/home.component.ts
@@ -9,9 +9,17 @@ export class HomeComponent implements OnInit {
 
   constructor(private src: AuthService) { }
   isUser: any;
+  charactersLeft: number = 120;
+  
   ngOnInit(): void {
     this.isUser = localStorage.getItem('user')
     this.isUser = JSON.parse(this.isUser)
 
+  }
+  keyup(textarea: any): void{
+    this.charactersLeft =  120 - textarea.value.length
+  }
+  clearTextArea(textarea: any):void{
+    textarea.value = ""
   }
 }
