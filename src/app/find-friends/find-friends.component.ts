6cc6ead33a1589e5cff5ac6d1bce6b3b8934433c
GoofyGalaxy374 <prophetyordanov@gmail.com>
2022-04-15 00:10:52 +0300

    The follow button now works on the find friends page

--- src/app/find-friends/find-friends.component.ts
+++ src/app/find-friends/find-friends.component.ts
@@ -13,11 +13,25 @@ export class FindFriendsComponent implements OnInit {
   constructor(private service: CRUDService,private route: ActivatedRoute, private http: HttpClient) { }
 
   public unfollowedUsers: any;
+  private userID: any;
   ngOnInit(): void {
-    let userID = this.route.snapshot.params['id'];
-    this.http.post('http://localhost:3000/getUnfollowedUsers/'+ userID, {userID: userID}).subscribe((res) => {
+    this.userID = this.route.snapshot.params['id'];
+    this.http.post('http://localhost:3000/getUnfollowedUsers/'+ this.userID, {userID: this.userID}).subscribe((res) => {
       this.unfollowedUsers = res
     })
+  }
+
+  FollowUser(card: any)
+  {
+    let targetUserID = card._id // ADD TO OWNER'S FOLLOWING
+    let currentUserID = this.userID // ADD TO TARGET'S FOLLOWERS
+
+    this.service.addFollower(targetUserID, currentUserID)
+    this.service.addFollowing(currentUserID, targetUserID)
+
+    console.log(currentUserID + 'is followed by' + targetUserID)
+
+    
   }
 
 
